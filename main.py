965c70896f09bbf2fdc2895458f03a074436535d
Anova <75539451+mosha7at@users.noreply.github.com>
2025-04-21 15:27:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ if __name__ == "__main__":
     # Set up logging
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=logging.INFO
+        level=logging.WARNING  # تغيير مستوى التسجيل إلى WARNING لتقليل الرسائل غير الضرورية
     )
+    
+    # تعطيل تسجيل الطلبات الناجحة من مكتبة httpx
+    logging.getLogger("httpx").setLevel(logging.WARNING)
     
     # Start the Telegram bot
     start_bot()
